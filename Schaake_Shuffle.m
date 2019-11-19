618ef9cd4be7c37cbfbbcac3cd1126010f55d008
Reggie Walters <reggiewalters@u.boisestate.edu>
2019-11-18 17:38:31 -0800

    Update Schaake_Shuffle.m

--- Schaake_Shuffle.m
+++ Schaake_Shuffle.m
@@ -5,7 +5,12 @@
 % Input dimensions are [M x N], where M = number of ensemble members and 
 % N = forecast days.
 % R. Walters, HHWP, Nov 2019
-
+%
+% Clark, M. P., S. Gangopadhyay, L. E. Hay, B. Rajagopalan, and R. L. Wilby, 2004: 
+% The Schaake shuffle: A method for reconstructing space–time variability in 
+% forecasted precipitation and temperature fields. J. Hydrometeor., 5, 243–262, 
+% doi:https://doi.org/10.1175/1525-7541(2004)005<0243:TSSAMF>2.0.CO;2
+%
 function X_ss = Schaake_Shuffle(X, Y)
 
 % % % Sort forecast input array X
@@ -14,12 +19,12 @@ Chi = sort(X);
 % % % Sort observed input array Y and get index vector B
 [~, B] = sort(Y);
 
-% % % Perform Schaake Shuffle for the re-ordered array of forecast ensemble
-%     members
-[n, L] = size(X);           % number of ensemble members, forecast length
-
+% % % Perform Schaake Shuffle for the re-ordered array of forecast ensemble members
+[n, L] = size(X);               % number of ensemble members, forecast length (ensemble #)
+nA = repmat((1:n)', 1, L);
 X_ss = nan.*ones(n,L);
+
 for i = 1:L
-    [~, ~, q] = intersect(repmat((1:n)', 1, n), B(:,i));
+    [~, ~, q] = intersect(nA, B(:,i));
     X_ss(:,i) = Chi(q,i);
 end
