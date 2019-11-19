b3c1bc47e848e92e08f3480ae5e39f7f0f492c7d
Reggie Walters <reggiewalters@u.boisestate.edu>
2019-11-18 17:01:17 -0800

    Update get_USGS.m

--- get_USGS.m
+++ get_USGS.m
@@ -6,6 +6,7 @@
 % R. Walters, HHWP, Aug 2018
 % with snippet(s) from R. Picklum
 % Updated 10/16/2018 to fix time array issue
+% Updated 11/15/2019 to address issues with new SFPUC web filter
 %
 %%% USAGE:
 %   >> get_USGS(siteNum, StartDate, EndDate)
@@ -28,50 +29,105 @@
 %
 function [elev, QV, dates, qual] = get_USGS(siteNum, StartDate, EndDate)
 
+% temporary certificate work-around
+options = weboptions;
+options.CertificateFilename=('');
+
 if ~ischar(siteNum)     siteNum = num2str(siteNum);     end
 if strcmpi(EndDate, 'now') == 1     EndDate = now;      end
-    
+
 dFormat  = 'yyyy-mm-dd';
+
+if ischar(StartDate)    StartDate = datenum(StartDate);     end
+if ischar(EndDate)      EndDate   = datenum(EndDate);       end
+    
 startStr = datestr(StartDate, dFormat);
 endStr   = datestr(EndDate, dFormat);
 
+nDays = EndDate-StartDate;
 
-% River Case:
-cb1 = '00060';    % discharge [cfs]
-cb2 = '00065';    % stage [ft]
-furl = ['https://waterdata.usgs.gov/ca/nwis/uv?cb_' cb1 '=on&amp&cb_' ...
-     cb2 '=on&format=rdb&period&begin_date=' startStr '&end_date=' endStr ...
-     '&site_no=' siteNum];
-a = urlread(furl);
-if numel(a) > 250
+if nDays < 119 && (floor(now) - StartDate) < 119
     
-    hLines = numel(strfind(a, '#')) + 2;    % number of headers
-    s = textscan(a, '%s %s %s %s %s %f %s %f %s', 'headerLines', hLines);
-    
-% Reservoir Case:
-else
-    cb1 = '00054';    % storage [acre-ft]
-    cb2 = '62614';    % elevation [ft]
-    
+    % River Case:
+    cb1 = '00060';    % discharge [cfs]
+    cb2 = '00065';    % stage [ft]
     furl = ['https://waterdata.usgs.gov/ca/nwis/uv?cb_' cb1 '=on&amp&cb_' ...
         cb2 '=on&format=rdb&period&begin_date=' startStr '&end_date=' endStr ...
         '&site_no=' siteNum];
-    a = urlread(furl);
-    hLines = numel(strfind(a, '#')) + 2;    % number of headers
-    s = textscan(a, '%s %d %s %s %s %f %s %f %s', 'headerLines', hLines);
+    a = webread(furl, options);
+    if numel(a) > 250
+        
+        hLines = numel(strfind(a, '#')) + 2;    % number of headers
+        s = textscan(a, '%s %s %s %s %s %f %s %f %s', 'headerLines', hLines);
+        
+        % Reservoir Case:
+    else
+        cb1 = '00054';    % storage [acre-ft]
+        cb2 = '62614';    % elevation [ft]
+        
+        furl = ['https://waterdata.usgs.gov/ca/nwis/uv?cb_' cb1 '=on&amp&cb_' ...
+            cb2 '=on&format=rdb&period&begin_date=' startStr '&end_date=' endStr ...
+            '&site_no=' siteNum];
+        a = webread(furl, options);
+        hLines = numel(strfind(a, '#')) + 2;    % number of headers
+        s = textscan(a, '%s %d %s %s %s %f %s %f %s', 'headerLines', hLines);
+    end
+    
+    % timing vector array
+    allDates = s{3};    allTimes = s{4};
+    DM = [allDates allTimes];
+    dates = zeros(1,size(DM,1));
+    for i = 1:size(DM,1)
+        d = strjoin(DM(i,:));
+        dates(i) = datenum(d);
+    end
+    dates = dates';
+    
+    % store output arrays
+    QV =    s{6};
+    elev =  s{8};
+    qual =  s{end};
+    
+elseif nDays >= 120 || (floor(now) - StartDate) >= 119
+    
+    % River Case:
+    cb1 = '00060';    % discharge [cfs]
+    cb2 = '00065';    % stage [ft]
+    furl = ['https://nwis.waterdata.usgs.gov/ca/nwis/uv?cb_' cb1 '=on&amp&cb_' ...
+        cb2 '=on&format=rdb&period&begin_date=' startStr '&end_date=' endStr ...
+        '&site_no=' siteNum];
+    a = webread(furl, options);
+    if numel(a) > 250
+        
+        hLines = numel(strfind(a, '#')) + 2;    % number of headers
+        s = textscan(a, '%s %s %s %s %s %f %s %f %s', 'headerLines', hLines);
+        
+        % Reservoir Case:
+    else
+        cb1 = '00054';    % storage [acre-ft]
+        cb2 = '62614';    % elevation [ft]
+        
+        furl = ['https://nwis.waterdata.usgs.gov/ca/nwis/uv?cb_' cb1 '=on&amp&cb_' ...
+            cb2 '=on&format=rdb&period&begin_date=' startStr '&end_date=' endStr ...
+            '&site_no=' siteNum];
+        a = webread(furl, options);
+        hLines = numel(strfind(a, '#')) + 2;    % number of headers
+        s = textscan(a, '%s %d %s %s %s %f %s %f %s', 'headerLines', hLines);
+    end
+    
+    % timing vector array
+    allDates = s{3};    allTimes = s{4};
+    DM = [allDates allTimes];
+    dates = zeros(1,size(DM,1));
+    for i = 1:size(DM,1)
+        d = strjoin(DM(i,:));
+        dates(i) = datenum(d);
+    end
+    dates = dates';
+    
+    % store output arrays
+    QV =    s{6};
+    elev =  s{8};
+    qual =  s{end};
+    
 end
-
-% timing vector array
-allDates = s{3};    allTimes = s{4};
-DM = [allDates allTimes];
-dates = zeros(1,size(DM,1));
-for i = 1:size(DM,1)
-    d = strjoin(DM(i,:));
-    dates(i) = datenum(d);
-end
-dates = dates';
-
-% store output arrays
-QV =    s{6};
-elev =  s{8};
-qual =  s{end};
