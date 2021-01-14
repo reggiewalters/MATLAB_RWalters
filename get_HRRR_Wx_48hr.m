91da3505da57b1f6749eb68d93244dc2b08e20e1
Reggie Walters <reggiewalters@u.boisestate.edu>
2021-01-14 08:34:04 -0800

    Update and rename get_HRRR_Wx_36hr.m to get_HRRR_Wx_48hr.m

--- get_HRRR_Wx_48hr.m
+++ get_HRRR_Wx_48hr.m
@@ -1,12 +1,13 @@
-function [Precip, Temp, Snow, time] = get_HRRR_Wx_36hr(sLat, sLon)
+function [Precip, Temp, Snow, RelH, time] = get_HRRR_Wx_48hr(sLat, sLon)
 %
 % function pulls precipitation/temperature/snow data from the High-Resolution Rapid
 % Refresh (HRRR) model. Resolution: 3km, hourly
-% HRRR integrates to 36 hours for 00/06/12/18z UTC cycles and for 18 hours 
+% HRRR integrates to 48 hours for 00/06/12/18z UTC cycles and for 18 hours
 % for all other cycles.
-% this function will force a download of the most recent 36-hour cycle and
+% this function will force a download of the most recent 48-hour cycle and
 % return time, precip, 2m air temp and snowfall
 % r. walters, hhwp, november 2018
+% updated september 2019 to add relative humidity
 %
 % \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 %
@@ -14,7 +15,7 @@ function [Precip, Temp, Snow, time] = get_HRRR_Wx_36hr(sLat, sLon)
 %       sLat: site latitude in decimal degrees, singular or array
 %             minimum: 21.1405 deg, maximum: 52.6133 deg)
 %
-%       sLon: site longitude in decimal degrees (negative values W of 
+%       sLon: site longitude in decimal degrees (negative values W of
 %             Greenwich), same dimension as sLat
 %             minimum: -134.0955 deg, maximum: -60.9365 deg
 %
@@ -25,6 +26,7 @@ function [Precip, Temp, Snow, time] = get_HRRR_Wx_36hr(sLat, sLon)
 %       Precip: surface total precipitation [inches]
 %       Temp: 2 m above ground temperature [F]
 %       Snow: surface total snowfall [inches]
+%       RH:   2 m relative humidity [%]
 %       time: hourly timestamps, corrected to local time zone
 %
 % \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
@@ -34,8 +36,8 @@ if length(sLat) ~= length(sLon)
     disp('input latitude and longitude arrays must be of same size!');
     return
 end
-    
-vars = {'apcpsfc', 'asnowsfc', 'tmp2m'};    % {precip [kg/m2], snowfall [m], temp [k]}
+
+vars = {'apcpsfc', 'asnowsfc', 'tmp2m', 'rh2m'};    % {precip [kg/m2], snowfall [m], temp [k], RH [%]}
 
 timeZone = 'America/Los_Angeles';           % hard-wired time zone locale
 T = datetime('today','TimeZone',timeZone);
@@ -47,7 +49,7 @@ else
 end
 
 date = datestr(now,'yyyymmdd');
-hrC = datevec(now); hrC = hrC(4);
+hrC = hour(datetime(datestr(now)));
 runC = hrC-dT;
 dFlag = 0;
 if hrC > (23+dT)
@@ -61,7 +63,7 @@ runD = runC - runV;
 runI = find(runD >= 0, 1);
 runC  = runV(runI);
 disp('---');
-disp('--- checking for most current 36-hr HRRR forecast ---');
+disp('--- searching for most current 48-hr HRRR forecast ---');
 disp('--- please wait ... ');
 
 while stop_flag == 0
@@ -71,8 +73,15 @@ while stop_flag == 0
         else
             R = num2str(runC);
         end
-        fullpath = ['http://nomads.ncep.noaa.gov/dods/hrrr/hrrr',date,'/hrrr_sfc.t',R,'z'];
-        time = ncread(fullpath,'time',[1],[Inf])+365;
+        fullpath = ['https://nomads.ncep.noaa.gov/dods/hrrr/hrrr',date,'/hrrr_sfc.t',R,'z'];
+        time = ncread(fullpath,'time',[1],[Inf]);
+        time = time + 365;
+%         dV = datevec(time);
+%         dV(:,1) = dV(:,1) + 1;
+%         t = datenum(dV);
+%         delta_t = mode(diff(t));
+%         t = t(1) : delta_t : t(end);
+%         time = t(1:length(time));
         stop_flag = 1;
     catch
         runC = runC-6;
@@ -80,12 +89,20 @@ while stop_flag == 0
     if runC < 0 && dFlag == 0
         stop_flag = 1;
         warning('no fx file found, please check path syntax');
+        return
     elseif runC < 0 && dFlag == 1
         try
             R = '18';
             date = datestr(now,'yyyymmdd');
-            fullpath = ['http://nomads.ncep.noaa.gov/dods/hrrr/hrrr',date,'/hrrr_sfc.t',R,'z'];
-            time = ncread(fullpath,'time',[1],[Inf])+365;
+            fullpath = ['https://nomads.ncep.noaa.gov/dods/hrrr/hrrr',date,'/hrrr_sfc.t',R,'z'];
+            time = ncread(fullpath,'time',[1],[Inf]);
+            time = time + 365;
+%             dV = datevec(time);
+%             dV(:,1) = dV(:,1) + 1;
+%             t = datenum(dV);
+%             delta_t = mode(diff(t));
+%             t = t(1) : delta_t : t(end);
+%             time = t(1:length(time));
             stop_flag = 1;
         catch
             stop_flag = 1;
@@ -98,9 +115,18 @@ end
 valid_str = ['forecast found! valid ' R 'Z ' datestr(datenum(date,'yyyymmdd'))];
 disp(valid_str)
 disp('Pulling Lat/Lon Info...');
-lat  = ncread(fullpath,'lat',[1],[Inf]);
-lon  = ncread(fullpath,'lon',[1],[Inf]);
-disp('Pulling Lat/Lon/Time Info... DONE');
+n = 0;
+while n<5
+    try
+        lat  = ncread(fullpath,'lat',[1],[Inf]);
+        lon  = ncread(fullpath,'lon',[1],[Inf]);
+        n=5;
+    catch
+        pause(2);
+        n = n+1;
+    end
+end
+disp('Pulling Lat/Lon Info... DONE');
 disp('---');
 
 % get the min/max lat/lon values to get an output matrix spanning the range
@@ -120,26 +146,31 @@ Lat = lat(iLat:iLat+latN-1);
 Lon = lon(iLon:iLon+lonN-1);
 
 %=========== read the data ================================================
-stop_flag = 0;  wait_Time = 2;   max_attempts = 5;   n=0;
+stop_flag = 0;  wait_Time = 3;   max_attempts = 7;   n=0;
 disp('--- Pulling precip/temperature forecast data ... ---');
 while stop_flag == 0
     n = n+1;
     disp(['attempt #: ' num2str(n)]);
-    try      
-        pause(0.25);
-        pcp = ncread(fullpath,vars{1},[iLon iLat 1],[lonN latN Inf]); 
+    try
+        
+        pause(1.0);
+        
+        pcp = ncread(fullpath,vars{1},[iLon iLat 1],[lonN latN Inf]);
         PPt = pcp./25.4;                    % convert km/m2 to in.
         PPt = permute(PPt,[2,1,3]);         % permute to correct matrix size (lat=rows,lon=cols)
-        pause(0.25);
+        pause(0.5);
         
-        sno = ncread(fullpath,vars{2},[iLon iLat 1],[lonN latN Inf]); 
+        sno = ncread(fullpath,vars{2},[iLon iLat 1],[lonN latN Inf]);
         sno = sno.*39.3701;                 % convert m to in.
         sno = permute(sno,[2,1,3]);         % same permutation as above
-        pause(0.25);
+        pause(0.5);
         
-        Ta = ncread(fullpath,vars{3},[iLon iLat 1],[lonN latN Inf]); 
+        Ta = ncread(fullpath,vars{3},[iLon iLat 1],[lonN latN Inf]);
         Ta = (Ta-273.15).*9./5+32;          % convert K to F
         Ta = permute(Ta,[2,1,3]);           % same permutation as for PPt
+        
+        rh = ncread(fullpath,vars{4},[iLon iLat 1],[lonN latN Inf]);
+        rh = permute(rh,[2,1,3]);           % same permutation
         
         disp('--- forecast retrieved! ---');
         stop_flag = 1;
@@ -157,9 +188,11 @@ end
 for i = 1:length(sLat)
     dLat = abs(Lat - sLat(i));      dLon = abs(Lon - sLon(i));
     Ri = find(dLat==min(dLat));     Ci = find(dLon==min(dLon));
-    Precip(:,i)  = PPt(Ri,Ci,:);         
+    Precip(:,i)  = PPt(Ri,Ci,:);
     Temp(:,i)    = Ta(Ri,Ci,:);
     Snow(:,i)    = sno(Ri,Ci,:);
+    RelH(:,i)    = rh(Ri,Ci,:);
 end
 
 time       = time + (dT/24);
+
